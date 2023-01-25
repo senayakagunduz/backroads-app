0783af994473048a61b63d5b74f0007f96c99acf
senayakagunduz <senayakgndz@gmail.com>
2023-01-25 09:04:32 +0300

    delete item (with filter method )button added

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,46 +1,58 @@
-import React from 'react'
-import Title from './Title'
-import {tours} from "../data"
+import React, { useState } from "react";
+import Title from "./Title";
+import { tours } from "../data";
 
 const Tours = () => {
+  const [tour, setTour] = useState(tours);
+
+  const removeItem = (id) => {
+    let newTours = tour.filter((item) => item.id !== id);
+    setTour(newTours);
+  };
+
   return (
     <section className="section" id="tours">
-        <Title title="featured" subTitle="tours"/>
+      <Title title="featured" subTitle="tours" />
       {/* <div className="section-title">
         <h2>featured <span>tours</span></h2>
       </div> */}
       <div className="section-center featured-center">
-        {
-          tours.map((tour)=>{
-            const {id, date,title, text,icon, time, money,img}=tour;
-            return (
-        <article className="tour-card" key={id}>
-          <div className="tour-img-container">
-            <img src={img} className="tour-img" alt="tour4" />
-            <p className="tour-date">{date}</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>{title}</h4>
-            </div>
-            <p>
-              {text}
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span><i className={icon}></i></span> china
-              </p>
-              <p>{time}</p>
-              <p>{money}</p>
-            </div>
-          </div>
-        </article>
-            )
-          })
-        }
+        {tour.map((item) => {
+          const { id, date, title, text, icon, time, money, img } = item;
+          return (
+            <article className="tour-card" key={id}>
+              <div className="tour-img-container">
+                <img src={img} className="tour-img" alt="tour4" />
+                <p className="tour-date">{date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{title}</h4>
+                </div>
+                <p>{text}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className={icon}></i>
+                    </span>{" "}
+                    china
+                  </p>
+                  <p>{time}</p>
+                  <p>{money}</p>
+                </div>
+              </div>
+              <div>
+                <button className="btn" onClick={() => removeItem(id)} style={{marginRight:'41px'}}>
+                  Remove item
+                </button>
+                <button className="btn" onClick={()=>setTour([])}>Remove all</button>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Tours
+export default Tours;
